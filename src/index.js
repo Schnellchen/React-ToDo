f06d51feaa7da4af80c8e204d87f94a080dce2c5
TestName <delf630@gmail.com>
2020-10-15 16:55:59 +0300

    Added input autofocus and autoclean

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM, {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 import './styles.css';
 
 
@@ -8,7 +8,7 @@ class Task extends React.Component {
         super(props);
         this.onClickRemove = this.onClickRemove.bind(this);
         this.onClickDone = this.onClickDone.bind(this);
-        this.onClickEdit = this.onClickEdit.bind(this);
+        //this.onClickEdit = this.onClickEdit.bind(this);
     }
 
     onClickRemove() {
@@ -21,24 +21,31 @@ class Task extends React.Component {
         this.props.taskDone(id);
     }
 
+    /*
     onClickEdit(event) {
+        let input = event.target;
+        //input.disabled = false;
         let id = this.props.item.id;
-        let text = event.target.value;
-        this.props.editTask(id,text);
+        let text = input.value;
+        alert(text);
+        //this.props.editTask(id,text);
+        //input.disabled = true;
     }
-
+     */
 
     render() {
-        let style = this.props.item.done ? "done" : "";
+        let style = this.props.item.done ? "task_done" : "";
+
         return(
-            <li className={"to-do-list li"}>
+            <li className={"to-do-list"}>
                 <div className={"to-do-list__item colors__item_navy"}>
                     <div className="task__manage">
-                        <input className="task__checkbox" checked={this.props.item.done} onClick = {this.onClickDone} type="checkbox"/>
+                        <input className="task__checkbox" checked={this.props.item.done} onClick={this.onClickDone} type="checkbox"/>
                     </div>
                     <div className="task__body" >
                         <p className={`task__text ${style}`}> {this.props.item.text}</p>
-                        <div className="removeIcon" onClick={this.onClickRemove}>X</div>
+                        <input value={this.props.item.text} />
+                        <div className="task__remove" onClick={this.onClickRemove}>X</div>
                     </div>
                 </div>
             </li>
@@ -91,7 +98,6 @@ class TasksList extends React.Component {
     }
 
     render() {
-        console.log("All done after", this.state.allDone);
         let tasks = this.props.tasks;
         let tasksDone = this.props.tasks.filter((item) => item.done === true);
 
@@ -134,15 +140,15 @@ class TasksList extends React.Component {
                 <div className="">Всего {all} пунктов Сделано {done} пунктов Осталось {undone} пунктов</div>
             </ul>
         );
-
     }
 }
 
 class NewTaskForm extends React.Component { // Компонент доска
     constructor(props) {
         super(props);
+        this.inputRef = React.createRef(); // (?) Так можно сделать автофокус?
         this.state = {
-            value: ''
+            value: '',
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -159,13 +165,18 @@ class NewTaskForm extends React.Component { // Компонент доска
         if (text){
             this.props.addTask(text);
         }
+        this.setState({value: ""});
         event.preventDefault(); // Предотвращение перезагрузки страницы при отправке формы
     }
+
+    // (?) Так можно сделать автофокус?
+
 
     render() {
         return (
             <form onSubmit = {this.handleSubmit} className="new-task">
-                <input type="text" onChange = {this.handleChange} placeholder="Add new item" className="new-task__input" maxLength="85" id="new-task__input"/>
+                <input ref={this.inputRef} autoFocus type="text" onChange = {this.handleChange} placeholder="Add new item"
+                       value={this.state.value} className="new-task__input" maxLength="85" id="new-task__input"/>
                     <div className="new-task__controls">
                         <button className="new-task__btn">Add</button>
                     </div>
@@ -224,22 +235,9 @@ class ToDoApp extends React.Component {
     }
 
     allTasksDone(allDone) {
-        // (?) лучше так
-        /*
-        let tasks = this.state.tasks.map((item) => {
-            return {
-                id: item.id,
-                text: item.text,
-                done: allDone,
-            }
-        });
-        */
-
-        // или так?
         this.state.tasks.forEach((item) => item.done = allDone);
         let tasks = this.state.tasks.slice();
         this.setState({tasks: tasks});
-
     }
 
     editTask(taskId, text) {
