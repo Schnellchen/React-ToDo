de342084c66969d63ac1e65e1b123a1d67a42498
TestName <delf630@gmail.com>
2020-10-16 11:29:16 +0300

    Double click to edit has been finally done as well as some improvements

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ import './styles.css';
 class Task extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isEdit: false,
+            value: "",
+        }
         this.onClickRemove = this.onClickRemove.bind(this);
         this.onClickDone = this.onClickDone.bind(this);
-        //this.onClickEdit = this.onClickEdit.bind(this);
+        this.onDoubleClickEdit = this.onDoubleClickEdit.bind(this);
+        // (?) Мб лучше сделать отдельный компонент?
+        this.handleChange = this.handleChange.bind(this);
+        this.onBlur = this.onBlur.bind(this);
+        this.onKeyDown = this.onKeyDown.bind(this);
+
     }
 
     onClickRemove() {
@@ -21,32 +30,67 @@ class Task extends React.Component {
         this.props.taskDone(id);
     }
 
-    /*
-    onClickEdit(event) {
-        let input = event.target;
-        //input.disabled = false;
-        let id = this.props.item.id;
-        let text = input.value;
-        alert(text);
-        //this.props.editTask(id,text);
-        //input.disabled = true;
+    onDoubleClickEdit() {
+        let value = this.props.item.text;
+        this.setState({value: value, isEdit: true});
     }
-     */
+
+    onBlur() {
+        console.log("im not focused");
+        let text = this.state.value.trim();
+        if (text) {
+            this.props.editTask(this.props.item.id, text);
+            this.setState({value: '', isEdit: false});
+        } else {
+            let id = this.props.item.id;
+            this.props.removeTask(id);
+            this.setState({value: '', isEdit: false});
+        }
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    onKeyDown(event) {
+        switch (event.key) {
+            case "Enter":
+                let text = this.state.value.trim();
+                if (text) {
+                    this.props.editTask(this.props.item.id, text);
+                    this.setState({value: '', isEdit: false});
+                } else {
+                    let id = this.props.item.id;
+                    this.props.removeTask(id);
+                    this.setState({value: '', isEdit: false});
+                }
+                break;
+            case "Escape":
+                this.setState({isEdit: false});
+                break;
+            default: return;
+        }
+    }
 
     render() {
         let style = this.props.item.done ? "task_done" : "";
 
+        let div = <div className="task__body" >
+            <p className={`task__text ${style}`}> {this.props.item.text}</p>
+            <div className="task__remove" onClick={this.onClickRemove}>X</div>
+        </div>;
+        let input = <input autoFocus value={this.state.value} onKeyDown={this.onKeyDown} onChange = {this.handleChange}
+                           onFocus={this.onFocus} onBlur={this.onBlur} />;
+        let block = this.state.isEdit ? input : div;
+
         return(
-            <li className={"to-do-list"}>
+            <li className={"to-do-list"} onDoubleClick={this.onDoubleClickEdit}>
                 <div className={"to-do-list__item colors__item_navy"}>
                     <div className="task__manage">
-                        <input className="task__checkbox" checked={this.props.item.done} onClick={this.onClickDone} type="checkbox"/>
+                        <input className="task__checkbox" checked={this.props.item.done} onClick={this.onClickDone}
+                               type="checkbox"/>
                     </div>
-                    <div className="task__body" >
-                        <p className={`task__text ${style}`}> {this.props.item.text}</p>
-                        <input value={this.props.item.text} />
-                        <div className="task__remove" onClick={this.onClickRemove}>X</div>
-                    </div>
+                    {block}
                 </div>
             </li>
         )
@@ -88,7 +132,7 @@ class TasksList extends React.Component {
         }
 
         let allDone = !this.state.allDone;
-        if (tasks.filter((item) => item.done === false).length > 0) {
+        if (tasks.some((item) => item.done === false)) {
             this.props.allTasksDone(true);
             this.setState({allDone: true});
         } else {
@@ -101,28 +145,32 @@ class TasksList extends React.Component {
         let tasks = this.props.tasks;
         let tasksDone = this.props.tasks.filter((item) => item.done === true);
 
-        let all = tasks.length;
-        let done = tasksDone.length;
-        let undone = tasks.length - tasksDone.length;
+        let allTasksCount = tasks.length;
+        let doneTasksCount = tasksDone.length;
+        let undoneTasksCount = tasks.length - tasksDone.length;
+
         switch (this.state.currentTasks) {
             case "all":
                 tasks = tasks.map((item) => {
                     return (
-                        <Task item = {item} id = {item.id} removeTask = {this.props.removeTask} taskDone = {this.props.taskDone}/>
+                        <Task item = {item} id = {item.id} removeTask = {this.props.removeTask}
+                              taskDone = {this.props.taskDone} editTask = {this.props.editTask}/>
                     )
                 });
                 break;
             case "done":
                 tasks = tasksDone.map((item) => {
                     return (
-                        <Task item = {item} id = {item.id} removeTask = {this.props.removeTask} taskDone = {this.props.taskDone}/>
+                        <Task item = {item} id = {item.id} removeTask = {this.props.removeTask}
+                              taskDone = {this.props.taskDone} editTask = {this.props.editTask}/>
                     )
                 });
                 break;
             case "undone":
                 tasks = this.props.tasks.filter((item) => item.done === false).map((item) => {
                     return (
-                        <Task item = {item} id = {item.id} removeTask = {this.props.removeTask} taskDone = {this.props.taskDone}/>
+                        <Task item = {item} id = {item.id} removeTask = {this.props.removeTask}
+                              taskDone = {this.props.taskDone} editTask = {this.props.editTask}/>
                     )
                 });
                 break;
@@ -137,7 +185,8 @@ class TasksList extends React.Component {
                 <div className="" onClick={this.onClickShowUndone}>Показать неотмеченные</div>
                 <div className="" onClick={this.onClickShowAll}>Показать все</div>
                 <div className="" onClick={this.onClickDoneAll}>Выделить все</div>
-                <div className="">Всего {all} пунктов Сделано {done} пунктов Осталось {undone} пунктов</div>
+                <div className="">Всего {allTasksCount } пунктов Сделано {doneTasksCount }
+                пунктов Осталось {undoneTasksCount } пунктов</div>
             </ul>
         );
     }
@@ -146,7 +195,6 @@ class TasksList extends React.Component {
 class NewTaskForm extends React.Component { // Компонент доска
     constructor(props) {
         super(props);
-        this.inputRef = React.createRef(); // (?) Так можно сделать автофокус?
         this.state = {
             value: '',
         };
@@ -169,13 +217,11 @@ class NewTaskForm extends React.Component { // Компонент доска
         event.preventDefault(); // Предотвращение перезагрузки страницы при отправке формы
     }
 
-    // (?) Так можно сделать автофокус?
-
 
     render() {
         return (
             <form onSubmit = {this.handleSubmit} className="new-task">
-                <input ref={this.inputRef} autoFocus type="text" onChange = {this.handleChange} placeholder="Add new item"
+                <input autoFocus type="text" onChange = {this.handleChange} placeholder="Add new item"
                        value={this.state.value} className="new-task__input" maxLength="85" id="new-task__input"/>
                     <div className="new-task__controls">
                         <button className="new-task__btn">Add</button>
@@ -192,7 +238,8 @@ class ToDoApp extends React.Component {
             tasks: [],
             counter: -1,
         }
-        this.addTask = this.addTask.bind(this); // Установка контекста этого родительского компонента для того, чтобы вызывать эти методы через пропсы в дочках
+        // Установка контекста этого родительского компонента для того, чтобы вызывать эти методы через пропсы в дочках
+        this.addTask = this.addTask.bind(this);
         this.removeTask = this.removeTask.bind(this);
         this.taskDone = this.taskDone.bind(this);
         this.editTask = this.editTask.bind(this);
@@ -215,18 +262,19 @@ class ToDoApp extends React.Component {
         console.log(this.state.tasks);
     }
 
+    // Удаление таска. Передается через TasksList в Task
     removeTask(taskId){
-        console.log(taskId); //   Через фильтр
        let tasks = this.state.tasks.filter((item) => item.id !== taskId);
        this.setState({tasks: tasks});
-        console.log(this.state.tasks);
     }
 
+    // Передается в TasksList
     removeDone(){
         let tasks = this.state.tasks.filter((item) => item.done !== true);
         this.setState({tasks: tasks});
     }
 
+    // Передается через TasksList в Task
     taskDone(taskId){
         let task = this.state.tasks.find((item) => item.id === taskId)
         task.done = !task.done;
@@ -234,12 +282,14 @@ class ToDoApp extends React.Component {
         this.setState({tasks: tasks});
     }
 
+    // Передается в TasksList
     allTasksDone(allDone) {
         this.state.tasks.forEach((item) => item.done = allDone);
         let tasks = this.state.tasks.slice();
         this.setState({tasks: tasks});
     }
 
+    // Передается через TasksList в Task
     editTask(taskId, text) {
         let task = this.state.tasks.find((item) => item.id === taskId)
         task.text = text;
@@ -247,12 +297,14 @@ class ToDoApp extends React.Component {
         this.setState({tasks: tasks});
     }
 
+    // Функция отрисовки элементов
     render() {
         return (
             <div className="wrapper">
                 <div className="to-do-app">
                     <NewTaskForm addTask = {this.addTask}/>
-                    <TasksList tasks = {this.state.tasks} removeTask = {this.removeTask} removeDone = {this.removeDone} taskDone = {this.taskDone}
+                    <TasksList tasks = {this.state.tasks} removeTask = {this.removeTask} removeDone = {this.removeDone}
+                               taskDone = {this.taskDone}
                                allTasksDone = {this.allTasksDone} editTask = {this.editTask}/>
                 </div>
             </div>
