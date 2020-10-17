a46bb2df2dc00b015f18bca09b4d69d36904fdfb
Schnellchen <delf97@mail.ru>
2020-10-17 23:26:12 +0300

    Design is almost done

--- src/index.js
+++ src/index.js
@@ -73,26 +73,36 @@ class Task extends React.Component {
     }
 
     render() {
-        let style = this.props.item.done ? "task_done" : "";
+        let style = this.props.item.done ? "task__text_done" : "";
 
-        let div = <div className="task__body" >
-            <p className={`task__text ${style}`}> {this.props.item.text}</p>
-            <div className="task__remove" onClick={this.onClickRemove}>X</div>
-        </div>
-        let input = <input autoFocus value={this.state.value} onKeyDown={this.onKeyDown} onChange = {this.handleChange}
-                           onFocus={this.onFocus} onBlur={this.onBlur} />;
+        let div = 
+        <React.Fragment>
+            <div className="task__manage">
+                <input type="checkbox" checked={this.props.item.done} onClick={this.onClickDone} className="task__checkbox"/>
+            </div>
+            <div className="task__body" >
+                <p className={`task__text ${style}`}>{this.props.item.text}</p>
+            </div>
+            <div className="task__manage">
+                <div className="task__remove" onClick={this.onClickRemove}>×</div>
+            </div>
+        </React.Fragment>
+
+        let input = <React.Fragment>
+            <div className="task__manage">      
+            </div>
+            <input className="task__edit" autoFocus value={this.state.value} onKeyDown={this.onKeyDown} onChange = {this.handleChange}
+            onFocus={this.onFocus} onBlur={this.onBlur} />
+        </React.Fragment>
+
         let block = this.state.isEdit ? input : div;
 
         return(
-            <li className={"task-list__li"} onDoubleClick={this.onDoubleClickEdit}>
-                <div className={"to-do-list__item"}>
-                    <div className="task__manage">
-                        <input className="task__checkbox" checked={this.props.item.done} onClick={this.onClickDone}
-                               type="checkbox"/>
+            <li className="to-do-list__item" onDoubleClick={this.onDoubleClickEdit}>
+                    <div className="task">
+                        {block}
                     </div>
-                    {block}
-                </div>
-            </li>
+                </li>
         )
     }
 }
@@ -102,13 +112,11 @@ class TasksList extends React.Component {
         super(props);
         this.state = {
             currentTasks: "all",
-            allDone: false,
         };
         this.onClickRemoveDone = this.onClickRemoveDone.bind(this);
         this.onClickShowDone = this.onClickShowDone.bind(this);
         this.onClickShowUndone = this.onClickShowUndone.bind(this);
         this.onClickShowAll = this.onClickShowAll.bind(this);
-        this.onClickDoneAll = this.onClickDoneAll.bind(this);
     }
 
     onClickRemoveDone() {
@@ -125,29 +133,14 @@ class TasksList extends React.Component {
         this.setState({currentTasks:"all"});
     }
 
-    onClickDoneAll() {
-        let tasks = this.props.tasks;
-        if (tasks.length === 0) {
-            return ;
-        }
-
-        let allDone = !this.state.allDone;
-        if (tasks.some((item) => item.done === false)) {
-            this.props.allTasksDone(true);
-            this.setState({allDone: true});
-        } else {
-            this.props.allTasksDone(allDone);
-            this.setState({allDone: allDone});
-        }
-    }
 
     render() {
         let tasks = this.props.tasks;
         let tasksDone = this.props.tasks.filter((item) => item.done === true);
-
-        let allTasksCount = tasks.length;
+        //let allTasksCount = tasks.length;
         let doneTasksCount = tasksDone.length;
         let undoneTasksCount = tasks.length - tasksDone.length;
+        let clearDoneStyle = (doneTasksCount > 0) ? "task-menu__clear" : "task-menu__clear_hidden";
 
         switch (this.state.currentTasks) {
             case "all":
@@ -178,16 +171,25 @@ class TasksList extends React.Component {
         }
 
         return (
-            <ul className="tasks-list">
-                <div onClick={this.onClickDoneAll}>Выделить все</div>
+            <React.Fragment>
+                <ul className="task-list">
                 {tasks}
-                <div className="tasks-list__btn" onClick={this.onClickRemoveDone}>Удалить все отмеченные</div>
-                <div className="tasks-list__btn" onClick={this.onClickShowDone}>Показать отмеченные</div>
-                <div className="tasks-list__btn" onClick={this.onClickShowUndone}>Показать неотмеченные</div>
-                <div className="tasks-list__btn" onClick={this.onClickShowAll}>Показать все</div>
-                <div className="">Всего {allTasksCount } пунктов Сделано {doneTasksCount }
-                пунктов Осталось {undoneTasksCount } пунктов</div>
             </ul>
+            <div className="task-menu">
+            <div className="task-menu__counter">
+                <p> {undoneTasksCount + " items left"} </p>
+            </div>
+            <div className="task-menu__filter">
+            <div className={(this.state.currentTasks === "all") ? "task-menu__btn task-menu__btn_chosen" : "task-menu__btn"} onClick={this.onClickShowAll}>All</div>
+            <div className={(this.state.currentTasks === "undone") ? "task-menu__btn task-menu__btn_chosen" : "task-menu__btn"}  onClick={this.onClickShowUndone}>Active</div>
+            <div className={(this.state.currentTasks === "done") ? "task-menu__btn task-menu__btn_chosen" : "task-menu__btn"}  onClick={this.onClickShowDone}>Completed</div>
+            </div>
+            <div className={clearDoneStyle}>
+                <div className="task-menu__btn-clear" onClick={this.onClickRemoveDone}>Clear completed</div>
+            </div>
+        </div>
+            </React.Fragment>
+                  
         );
     }
 }
@@ -197,9 +199,11 @@ class NewTaskForm extends React.Component { // Компонент доска
         super(props);
         this.state = {
             value: '',
+            allDone: false,
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.onClickDoneAll = this.onClickDoneAll.bind(this);
     }
 
     //Меняет состояние каждый раз при изменении input
@@ -213,19 +217,40 @@ class NewTaskForm extends React.Component { // Компонент доска
         if (text){
             this.props.addTask(text);
         }
-        this.setState({value: ""});
+        this.setState({value: ''});
         event.preventDefault(); // Предотвращение перезагрузки страницы при отправке формы
     }
 
+    onClickDoneAll() {
+        let tasks = this.props.tasks;
+        if (tasks.length === 0) {
+            return ;
+        }
+
+        
+        if (tasks.some((item) => item.done === false)) {
+            this.props.allTasksDone(true);
+            this.setState({allDone: true});
+        } else {
+            this.props.allTasksDone(false);
+            this.setState({allDone: false});
+        }
+    }
 
     render() {
+        console.log((this.state.allDone && !this.props.tasks.some((item) => item.done === false)) && this.props.tasks.length > 0)
+        let selectorStyle = (this.state.allDone && !this.props.tasks.some((item) => item.done === false) && this.props.tasks.length > 0) ? "task-selector__btn task-selector__btn_chosen" : "task-selector__btn";
         return (
             <form onSubmit = {this.handleSubmit} className="new-task">
-                <input autoFocus type="text" onChange = {this.handleChange} placeholder="Add new item"
-                       value={this.state.value} className="new-task__input" maxLength="85" id="new-task__input"/>
-                    <div className="new-task__controls">
-                        <button className="new-task__btn">Add</button>
-                    </div>
+                <div className="task-selector">
+                    <p onClick={this.onClickDoneAll} className={selectorStyle}>❯</p>
+                </div>
+                <input placeholder="What needs to be done?" 
+                className="new-task__input" maxlength="85" autoFocus type="text" onChange = {this.handleChange}
+                value={this.state.value}/>
+                <div className="new-task__controls">
+                    <button className="new-task__btn" id="new-task__btn">Add</button>
+                </div>
             </form>
         );
     }
@@ -301,11 +326,13 @@ class ToDoApp extends React.Component {
     render() {
         return (
             <div className="wrapper">
+                <header className="header">
+                        <h1 className="header__text">todos</h1>
+                    </header>
                 <div className="to-do-app">
-                    <NewTaskForm addTask = {this.addTask}/>
-                    <TasksList tasks = {this.state.tasks} removeTask = {this.removeTask} removeDone = {this.removeDone}
-                               taskDone = {this.taskDone}
-                               allTasksDone = {this.allTasksDone} editTask = {this.editTask}/>
+            <NewTaskForm tasks = {this.state.tasks} addTask = {this.addTask} allTasksDone = {this.allTasksDone}/>
+            <TasksList tasks = {this.state.tasks} removeTask = {this.removeTask} removeDone = {this.removeDone}
+                       taskDone = {this.taskDone} editTask = {this.editTask}/>
                 </div>
             </div>
         );
@@ -318,4 +345,3 @@ ReactDOM.render(
     <ToDoApp />,
     document.getElementById('root')
 );
-
