87383f2806400ad32456b4f5c2c738fb5b520c13
Schnellchen <delf97@mail.ru>
2021-10-19 19:31:14 +0300

    Updates

---
+++ precache-manifest.e0eb4a8c356865c149bb8405f066e8ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61416abc29b383310951c3133b984674",
+    "url": "/React-ToDo/index.html"
+  },
+  {
+    "revision": "0f868c9fd02d088218be",
+    "url": "/React-ToDo/static/css/main.a9448209.chunk.css"
+  },
+  {
+    "revision": "69ac79973eda79f4491b",
+    "url": "/React-ToDo/static/js/2.24946cc6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-ToDo/static/js/2.24946cc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f868c9fd02d088218be",
+    "url": "/React-ToDo/static/js/main.ec4e119a.chunk.js"
+  },
+  {
+    "revision": "96373b6eb283c66e3bbc",
+    "url": "/React-ToDo/static/js/runtime-main.c8a8de8e.js"
+  }
+]);
